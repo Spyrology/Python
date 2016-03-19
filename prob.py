4c87fc9adcb2e0a095811d9c219b8df73b7b36c2
Spyros Karidis <spyrology@gmail.com>
2016-03-19 17:06:25 -0400

    plot distribution, print chai-ssquare value

--- prob.py
+++ prob.py
@@ -5,24 +5,24 @@ import matplotlib.pyplot as plt
 
 x = [1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 3, 4, 4, 4, 4, 5, 6, 6, 6, 7, 7, 7, 7, 7, 7, 7, 7, 8, 8, 9, 9]
 
-c = collections.Counter(x)
+# c = collections.Counter(x)
 
-print(c)
+# print(c)
 
-# calculate the number of instances in the list
-count_sum = sum(c.values())
+# # calculate the number of instances in the list
+# count_sum = sum(c.values())
 
-for k,v in c.iteritems():
-  print("The frequency of number " + str(k) + " is " + str(float(v) / count_sum))
+# for k,v in c.iteritems():
+#   print("The frequency of number " + str(k) + " is " + str(float(v) / count_sum))
 
 
-plt.boxplot(x)
-plt.show()
-plt.savefig('boxtest.png')
+# plt.boxplot(x)
+# plt.show()
+# plt.savefig('boxtest.png')
 
-plt.hist(x, histtype='bar')
-plt.show()
-plt.savefig('histogram.png')
+# plt.hist(x, histtype='bar')
+# plt.show()
+# plt.savefig('histogram.png')
 
 plt.figure()
 test_data = np.random.normal(size=1000)   
